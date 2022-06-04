bcf354a6215d41053c42636ab8309474d44db5ae
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-03 17:34:41 -0700

    added ec2 instance resource

--- instance.tf
+++ instance.tf
@@ -26,10 +26,12 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 #EC2 instance resource block
-resource "aws_instance" "main-ubuntu-ec2" {
+resource "aws_instance" "main-ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  security_groups = [ aws_security_group.allow_ssh.id ]
+  subnet_id = aws_subnet.public_subnet[0].id
+  associate_public_ip_address = true
+  security_groups = ["${aws_security_group.allow_ssh.id}"]
   key_name = var.key_name
 
   tags = {
