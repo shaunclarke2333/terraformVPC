2c897479d53d2f9af7f2252ccab4ffd4bd8d72f4
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-05 17:42:57 -0700

    removed inbound rule for port 443

--- instance.tf
+++ instance.tf
@@ -20,14 +20,6 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
 
   }
-  ingress {
-    description = "https to VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
 
   egress {
     from_port   = 0
