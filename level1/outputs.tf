eeb1074732096507dac0daf12373f3fb666f75e3
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-08 14:39:05 -0700

    formatted outputs.tf

--- level1/outputs.tf
+++ level1/outputs.tf
@@ -2,12 +2,12 @@
 output "main-vpc-id" {
   description = "will be used by devices that depend on the vpc ID"
   value       = aws_vpc.my-main-vpc.id
-  sensitive = true
+  sensitive   = true
 }
 
 # Output for main-public0 subnet id
 output "main-public0-subnet" {
   description = "will be used by devices that depend on main-public0 subnet ID"
   value       = aws_subnet.public_subnet[0].id
-  sensitive = true
+  sensitive   = true
 }
