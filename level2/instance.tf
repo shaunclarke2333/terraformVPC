87e36a18e473e69fa809b44b13e5c716e1d773fb
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-12 10:22:24 -0700

    moved security groups and vpc data source to their own files

--- level2/instance.tf
+++ level2/instance.tf
@@ -9,56 +9,6 @@ data "terraform_remote_state" "main-vpc" {
   }
 }
 
-# Security group for this EC2 instance to allow ssh inbound on port 22
-resource "aws_security_group" "allow_ssh" {
-  name        = "ec2-instance"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = data.terraform_remote_state.main-vpc.outputs.main-vpc-id
-
-  ingress {
-    description = "ssh to VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["70.95.138.224/32"]
-
-  }
-  ingress {
-    description = "http to VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "main-allows-ssh"
-  }
-}
-# Data source for aws_ami 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["aws-marketplace"]
-}
-
 # EC2 instance resource block
 resource "aws_instance" "main-ec2" {
   ami                         = data.aws_ami.ubuntu.id
