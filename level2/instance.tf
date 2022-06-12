2534a73391075e885686dab75128e51a8c9fa746
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-12 13:39:29 -0700

    added load balancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -1,12 +1,17 @@
-#This remote data source is for main vpc and main-public0 id
-data "terraform_remote_state" "main-vpc" {
-  backend = "s3"
-
-  config = {
-    bucket = "main-backend-state-bucket"
-    key    = "level1/terraform.tfstate"
-    region = "us-east-1"
+# Data source for aws_ami 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["aws-marketplace"]
 }
 
 # EC2 instance resource block
@@ -14,7 +19,7 @@ resource "aws_instance" "main-ec2" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   subnet_id                   = data.terraform_remote_state.main-vpc.outputs.main-private-subnet[0].id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   vpc_security_group_ids      = ["${aws_security_group.allow_ssh.id}"]
   key_name                    = var.key_name
   user_data                   = <<-EOF
--- level2/vpc-data-source.tf
+++ level2/vpc-data-source.tf
@@ -1,15 +1,10 @@
-# Data source for aws_ami 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#This remote data source is for main vpc and main-public0 id
+data "terraform_remote_state" "main-vpc" {
+  backend = "s3"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  config = {
+    bucket = "main-backend-state-bucket"
+    key    = "level1/terraform.tfstate"
+    region = "us-east-1"
   }
-
-  owners = ["aws-marketplace"]
 }
