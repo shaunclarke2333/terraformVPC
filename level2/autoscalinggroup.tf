aee3174bd408159953c9aad39a04c73b38a170ff
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-20 08:01:04 -0700

    made formatting changes

--- level2/autoscalinggroup.tf
+++ level2/autoscalinggroup.tf
@@ -4,11 +4,12 @@ resource "aws_autoscaling_group" "main-autoscaling-group" {
   max_size                  = 5
   min_size                  = 2
   health_check_grace_period = 200
-  health_check_type         = "ELB"
+  health_check_type         = "EC2"
   desired_capacity          = 4
   force_delete              = true
   vpc_zone_identifier       = [for subnet in data.terraform_remote_state.level1-main-vpc.outputs.main-private-subnet : subnet.id]
   target_group_arns         = [aws_lb_target_group.main-target-group.arn]
+
   launch_template {
     id      = aws_launch_template.main-launch-temp.id
     version = "$Latest"
@@ -16,12 +17,8 @@ resource "aws_autoscaling_group" "main-autoscaling-group" {
 
   tag {
     key                 = "name"
-    value               = "autoscaled-ubuntu-instance"
+    value               = "main-autoscaled-ubuntu-instance"
     propagate_at_launch = true
-  }
-
-  lifecycle {
-    ignore_changes = [load_balancers, target_group_arns]
   }
 }
 
--- level2/launchtemplate.tf
+++ level2/launchtemplate.tf
@@ -4,15 +4,11 @@ resource "aws_launch_template" "main-launch-temp" {
   image_id      = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
+
   vpc_security_group_ids = [
     "${aws_security_group.main-elb-tcp80.id}",
     "${aws_security_group.allow_ssh.id}"
   ]
-  #   user_data = <<-EOF
-  #     #!/bin/bash
-  #     sudo apt update -y
-  #     sudo apt install apache2 -y
-  #   EOF
 
   tag_specifications {
     resource_type = "instance"
