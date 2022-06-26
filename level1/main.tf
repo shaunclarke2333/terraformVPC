17987af0634f1625d732735247b9170315b6e636
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-26 13:12:46 -0700

    redeployed level1 VPC infrastructure with modules

--- level1/main.tf
+++ level1/main.tf
@@ -34,9 +34,9 @@ module "main_public_subnet" {
   source            = "../mods/modules/subnet"
   availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id            = module.my-main-vpc.main-vpc-id
-  subnet_cidr         = var.public_subnet_cidr[count.index]
+  subnet_cidr       = var.public_subnet_cidr[count.index]
 
-  name = "${count.index}main-public"
+  name = "${count.index}-main-public"
 }
 
 # private subnets in my-main-vpc
@@ -46,9 +46,9 @@ module "main_private_subnet" {
   source            = "../mods/modules/subnet"
   availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id            = module.my-main-vpc.main-vpc-id
-  subnet_cidr         = var.private_subnet_cidr[count.index]
-  name = "${count.index}-main-private"
- 
+  subnet_cidr       = var.private_subnet_cidr[count.index]
+  name              = "${count.index}-main-private"
+
 }
 
 # Internet gateway to allow public subents to access the internet
@@ -81,13 +81,13 @@ module "main_nat_gateway" {
 
 # Public route table to route to the internet gateway
 module "main_public_route_table" {
-  source = "../mods/modules/route_table"
-  vpc_id = module.my-main-vpc.main-vpc-id
+  source     = "../mods/modules/route_table"
+  vpc_id     = module.my-main-vpc.main-vpc-id
   cidr_block = "0.0.0.0/0"
   gateway_id = module.my_internet_gateway.internet_gateway_id
 
 
-  name = "public"
+  name = "main-public"
 }
 
 # Associating public route table with public subnets
@@ -104,13 +104,13 @@ module "main_public_route_table_association" {
 module "main_private_route_table" {
   count = length(var.private_subnet_cidr)
 
-  source = "../mods/modules/nat_route_table"
-  vpc_id = module.my-main-vpc.main-vpc-id
+  source         = "../mods/modules/nat_route_table"
+  vpc_id         = module.my-main-vpc.main-vpc-id
   cidr_block     = "0.0.0.0/0"
   nat_gateway_id = module.main_nat_gateway[count.index].nat_gateway_output.id
 
 
-  name = "${count.index}-private"
+  name = "${count.index}-main-private"
 }
 
 # Associating private route table with private subnets
