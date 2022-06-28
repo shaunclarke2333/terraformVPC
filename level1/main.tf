ad1761f199e668d7e9e20a67e62bc768927224db
Shaun Clarke <shaunclarke43@gmail.com>
2022-06-27 19:54:05 -0700

    building level one from modules

--- level1/main.tf
+++ level1/main.tf
@@ -13,3 +13,25 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+#Declaring availability zone data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Complete VPC deployment from VPC module
+module "main-vpc" {
+  count = length(var.public_subnet_cidr)
+
+  source = "../modules/vpc"
+
+  # my_vpc aws vpc resource 
+  vpc_cidr_block = "10.0.0.0/16"
+  vpc_tag_name = "main"
+
+  # Public subnets in my-main-vpc
+  public_availability_zone = data.aws_availability_zones.available.names[count.index]
+
+
+  
+}
